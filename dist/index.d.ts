18dbd95db68064f6c93b8d35cc7cbd629fcd83a1
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2024-01-14 12:32:57 +0300

    #25 build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
 import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
 import type { IRouteMeta } from '@/types';
 declare function updateMetaTag(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext, metas: Array<IRouteMeta>): void;
-declare const MetaTagUpdater: {
+export declare const MetaTagUpdater: {
     update: typeof updateMetaTag;
 };
 export default MetaTagUpdater;
